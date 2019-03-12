ab46375726452570053243dac32a0221808eb245
Wouter De Rijck <wouter.derijck@kb.vlaanderen.be>
2019-03-12 16:28:26 +0100

    fix: use the supplied ILoggerFactory by configuration from host

--- src/Be.Vlaanderen.Basisregisters.Projector/Modules/ProjectorModule.cs
+++ src/Be.Vlaanderen.Basisregisters.Projector/Modules/ProjectorModule.cs
@@ -4,16 +4,25 @@ namespace Be.Vlaanderen.Basisregisters.Projector.Modules
     using System.Reflection;
     using Autofac;
     using ConnectedProjections;
+    using Microsoft.Extensions.Logging;
     using Module = Autofac.Module;
 
     public class ProjectorModule : Module
     {
+        private readonly ILoggerFactory _loggerFactory;
+
+        public ProjectorModule(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+
         protected override void Load(ContainerBuilder builder)
         {
-            builder
-                .RegisterType<ConnectedProjectionsManager>()
+            builder.RegisterType<ConnectedProjectionsManager>()
                 .FindConstructorsWith(AllowNonPublicConstructor)
+                .WithParameter(new Parameter<ILoggerFactory>(_loggerFactory))
                 .AsSelf()
+                .As<IConnectedProjectionEventBus>()
                 .SingleInstance();
         }
 
@@ -23,6 +32,13 @@ namespace Be.Vlaanderen.Basisregisters.Projector.Modules
                 BindingFlags.Instance |
                 BindingFlags.Public |
                 BindingFlags.NonPublic);
+        }
+        
+        private class Parameter<T> : TypedParameter
+        {
+            public Parameter(T value)
+                : base(typeof(T), value)
+            { }
         }
     }
 }
