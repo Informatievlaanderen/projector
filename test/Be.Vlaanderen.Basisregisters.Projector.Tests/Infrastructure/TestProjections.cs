feed252339640acb9e28a0709df451e37c6735c9
Wouter De Rijck <wouter.derijck@kb.vlaanderen.be>
2019-03-21 13:45:14 +0100

    chore: add test project

---
+++ test/Be.Vlaanderen.Basisregisters.Projector.Tests/Infrastructure/TestProjections.cs
@@ -0,0 +1,7 @@
+namespace Be.Vlaanderen.Basisregisters.Projector.Tests.Infrastructure
+{
+    using ProjectionHandling.Connector;
+
+    public class Projections : ConnectedProjection<ProjectionContext>
+    { }
+}
