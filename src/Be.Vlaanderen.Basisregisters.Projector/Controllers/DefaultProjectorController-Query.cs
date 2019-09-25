6ace0bcff934c33a863a5c713b5e6a6e84e8c918
David Cumps <david@cumps.be>
2019-09-25 10:55:52 +0200

    fix: allow reserved column names

--- src/Be.Vlaanderen.Basisregisters.Projector/Controllers/DefaultProjectorController-Query.cs
+++ src/Be.Vlaanderen.Basisregisters.Projector/Controllers/DefaultProjectorController-Query.cs
@@ -18,18 +18,19 @@ namespace Be.Vlaanderen.Basisregisters.Projector.Controllers
             [FromRoute] string schema,
             [FromRoute] string table)
         {
-            //https://stackoverflow.com/a/30152027/412692
+            // https://stackoverflow.com/a/30152027/412692
             var tableNameRegex = new Regex(@"^[\p{L}_][\p{L}\p{N}@$#_]{0,127}$");
-            //https://stackoverflow.com/a/4978062/412692
+
+            // https://stackoverflow.com/a/4978062/412692
             var columnNameRegex = new Regex(@"^[a-zA-Z_][a-zA-Z0-9_]*$");
 
             if (!tableNameRegex.IsMatch(schema) || !tableNameRegex.IsMatch(table) || !_connectionStringBySchema.ContainsKey(schema.ToUpperInvariant()))
-                return BadRequest("Invalid schema or table name");
+                return BadRequest("Invalid schema or table name.");
 
             if (Request.Query.Count == 0)
-                return BadRequest("Please specify a filter in the query string");
+                return BadRequest("Please specify a filter in the query string.");
 
-            var select = new StringBuilder($"SELECT * FROM {schema}.{table} WHERE ");
+            var select = new StringBuilder($"SELECT * FROM [{schema}].[{table}] WHERE ");
 
             var a = Request.Query
                 .ToDictionary(x => x.Key, y => (object)y.Value.ToString())
@@ -38,9 +39,9 @@ namespace Be.Vlaanderen.Basisregisters.Projector.Controllers
             foreach (var keyValue in Request.Query)
             {
                 if (!columnNameRegex.IsMatch(keyValue.Key))
-                    return BadRequest("Invalid query parameter");
+                    return BadRequest("Invalid query parameter.");
 
-                select.Append($"{keyValue.Key}=@{keyValue.Key} AND ");
+                select.Append($"[{keyValue.Key}] = @{keyValue.Key} AND ");
             }
 
             select.Length -= 4;
@@ -56,7 +57,8 @@ namespace Be.Vlaanderen.Basisregisters.Projector.Controllers
                 catch (SqlException ex)
                 {
                     if (ex.Number == 207)
-                        return BadRequest("Invalid query parameter");
+                        return BadRequest("Invalid query parameter.");
+
                     throw;
                 }
             }
@@ -65,8 +67,6 @@ namespace Be.Vlaanderen.Basisregisters.Projector.Controllers
         }
 
         protected void RegisterConnectionString(string schema, string connectionString)
-        {
-            _connectionStringBySchema.Add(schema.ToUpperInvariant(), connectionString);
-        }
+            => _connectionStringBySchema.Add(schema.ToUpperInvariant(), connectionString);
     }
 }
--- src/Be.Vlaanderen.Basisregisters.Projector/DictionaryExtensions.cs
+++ src/Be.Vlaanderen.Basisregisters.Projector/DictionaryExtensions.cs
@@ -9,40 +9,46 @@ namespace Be.Vlaanderen.Basisregisters.Projector
         public static ExpandoObject ToExpando(this IDictionary<string, object> dictionary)
         {
             var expando = new ExpandoObject();
-            var expandoDic = (IDictionary<string, object>)expando;
+            var expandoDictionary = (IDictionary<string, object>)expando;
 
             // go through the items in the dictionary and copy over the key value pairs)
             foreach (var kvp in dictionary)
             {
-                // if the value can also be turned into an ExpandoObject, then do it!
-                if (kvp.Value is IDictionary<string, object>)
+                switch (kvp.Value)
                 {
-                    var expandoValue = ((IDictionary<string, object>)kvp.Value).ToExpando();
-                    expandoDic.Add(kvp.Key, expandoValue);
-                }
-                else if (kvp.Value is ICollection)
-                {
-                    // iterate through the collection and convert any strin-object dictionaries
-                    // along the way into expando objects
-                    var itemList = new List<object>();
-                    foreach (var item in (ICollection)kvp.Value)
+                    // if the value can also be turned into an ExpandoObject, then do it!
+                    case IDictionary<string, object> objectDictionary:
                     {
-                        if (item is IDictionary<string, object>)
-                        {
-                            var expandoItem = ((IDictionary<string, object>)item).ToExpando();
-                            itemList.Add(expandoItem);
-                        }
-                        else
-                        {
-                            itemList.Add(item);
-                        }
+                        var expandoValue = objectDictionary.ToExpando();
+                        expandoDictionary.Add(kvp.Key, expandoValue);
+                        break;
                     }
 
-                    expandoDic.Add(kvp.Key, itemList);
-                }
-                else
-                {
-                    expandoDic.Add(kvp);
+                    case ICollection value:
+                    {
+                        // iterate through the collection and convert any string-object dictionaries
+                        // along the way into expando objects
+                        var itemList = new List<object>();
+                        foreach (var item in value)
+                        {
+                            if (item is IDictionary<string, object> objectDictionary)
+                            {
+                                var expandoItem = objectDictionary.ToExpando();
+                                itemList.Add(expandoItem);
+                            }
+                            else
+                            {
+                                itemList.Add(item);
+                            }
+                        }
+
+                        expandoDictionary.Add(kvp.Key, itemList);
+                        break;
+                    }
+
+                    default:
+                        expandoDictionary.Add(kvp);
+                        break;
                 }
             }
 
