3620e8929ff7ae0e9d4892fbbf72bb4caa7155e4
David Cumps <david@cumps.be>
2020-10-20 11:39:23 +0200

    style: newline

--- src/Be.Vlaanderen.Basisregisters.Projector/Internal/Exceptions/StreamGapDetectedException.cs
+++ src/Be.Vlaanderen.Basisregisters.Projector/Internal/Exceptions/StreamGapDetectedException.cs
@@ -7,7 +7,6 @@ namespace Be.Vlaanderen.Basisregisters.Projector.Internal.Exceptions
     internal class StreamGapDetectedException : Exception
     {
         public StreamGapDetectedException(IEnumerable<long> missingPositions, ConnectedProjectionName projectionName)
-            : base($"Stream does not contain messages at positions [{string.Join(',', missingPositions)}] for projection {projectionName}")
-        { }
+            : base($"Stream does not contain messages at positions [{string.Join(',', missingPositions)}] for projection {projectionName}") { }
     }
 }
