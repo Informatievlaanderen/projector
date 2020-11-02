15a87a7e9a7f48b3a9c6e16c3baf1158891b173c
David Cumps <david@cumps.be>
2020-11-02 11:34:34 +0100

    style: formatting

--- src/Be.Vlaanderen.Basisregisters.Projector/ConnectedProjections/ConnectedProjectionSettings.cs
+++ src/Be.Vlaanderen.Basisregisters.Projector/ConnectedProjections/ConnectedProjectionSettings.cs
@@ -5,18 +5,19 @@ namespace Be.Vlaanderen.Basisregisters.Projector.ConnectedProjections
 
     public class ConnectedProjectionSettings : IConnectedProjectionSettings
     {
+        public int CatchUpPageSize { get; }
+        
+        public MessageHandlingRetryPolicy RetryPolicy { get; }
+
         internal ConnectedProjectionSettings(int catchUpPageSize, MessageHandlingRetryPolicy retryPolicy)
         {
             if (catchUpPageSize < 1)
                 throw new ArgumentException($"{nameof(catchUpPageSize)} has to be at least 1");
+                
             CatchUpPageSize = catchUpPageSize;
-
             RetryPolicy = retryPolicy ?? throw new ArgumentNullException(nameof(retryPolicy));
         }
-
-        public int CatchUpPageSize { get; }
-        public MessageHandlingRetryPolicy RetryPolicy { get; }
-
+        
         public static ConnectedProjectionSettings Default
             => new ConnectedProjectionSettingsConfigurator().CreateSettings();
 
