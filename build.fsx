a98fc91a18f294152049c3960bcf6102eeb8c60d
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2020-12-18 16:25:18 +0100

    fix: move to 5.0.1

--- build.fsx
+++ build.fsx
@@ -1,28 +1,44 @@
 #r "paket:
-version 5.241.6
+version 6.0.0-beta8
 framework: netstandard20
 source https://api.nuget.org/v3/index.json
-nuget Be.Vlaanderen.Basisregisters.Build.Pipeline 4.2.3 //"
+nuget Be.Vlaanderen.Basisregisters.Build.Pipeline 5.0.1 //"
 
 #load "packages/Be.Vlaanderen.Basisregisters.Build.Pipeline/Content/build-generic.fsx"
 
 open Fake.Core
 open Fake.Core.TargetOperators
+open Fake.IO.FileSystemOperators
 open ``Build-generic``
 
 let assemblyVersionNumber = (sprintf "%s.0")
 let nugetVersionNumber = (sprintf "%s")
 
-let build = buildSolution assemblyVersionNumber
-let publish = publishSolution assemblyVersionNumber
+let buildSource = build assemblyVersionNumber
+let buildTest = buildTest assemblyVersionNumber
+let publishSource = publish assemblyVersionNumber
 let pack = packSolution nugetVersionNumber
 
 supportedRuntimeIdentifiers <- [ "linux-x64" ]
 
 // Library ------------------------------------------------------------------------
-Target.create "Lib_Build" (fun _ -> build "Be.Vlaanderen.Basisregisters.Projector")
-Target.create "Lib_Test" (fun _ -> testSolution "Be.Vlaanderen.Basisregisters.Projector")
-Target.create "Lib_Publish" (fun _ -> publish "Be.Vlaanderen.Basisregisters.Projector")
+Target.create "Lib_Build" (fun _ ->
+    buildSource "Be.Vlaanderen.Basisregisters.Projector"
+    buildTest "Be.Vlaanderen.Basisregisters.Projector.Tests"
+    buildTest "Be.Vlaanderen.Basisregisters.Projector.TestProjections"
+)
+
+Target.create "Lib_Test" (fun _ ->
+    [
+        //"test" @@ "Be.Vlaanderen.Basisregisters.Projector.Tests"
+        "test" @@ "Be.Vlaanderen.Basisregisters.Projector.TestProjections"
+    ] |> List.iter testWithDotNet
+)
+
+Target.create "Lib_Publish" (fun _ ->
+    publishSource "Be.Vlaanderen.Basisregisters.Projector"
+)
+
 Target.create "Lib_Pack" (fun _ -> pack "Be.Vlaanderen.Basisregisters.Projector")
 
 // --------------------------------------------------------------------------------
