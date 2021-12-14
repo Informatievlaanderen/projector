0c3e2bb13e8f2569868cb9283874110225f00b74
Luk Vermeulen <luk.vermeulen@gmail.com>
2021-12-14 09:09:38 +0100

    fix: add other registries

--- src/Be.Vlaanderen.Basisregisters.Projector/Controllers/DefaultProjectorController-Query.cs
+++ src/Be.Vlaanderen.Basisregisters.Projector/Controllers/DefaultProjectorController-Query.cs
@@ -48,36 +48,4 @@ namespace Be.Vlaanderen.Basisregisters.Projector.Controllers
         protected void RegisterConnectionString(string schema, string connectionString)
             => _connectionStringBySchema.Add(schema.ToUpperInvariant(), connectionString);
     }
-
-    internal class EventDatabaseInfo
-    {
-        public string ExternalId { get; }
-        public string InternalId { get; }
-        public string DetailSchemaName { get; }
-        public string DetailTableName { get; }
-        public string DetailJoinColumnName { get; }
-        public string MainSchemaName { get; }
-        public string MainTableName { get; }
-        public string MainJoinColumnName { get; }
-
-        public EventDatabaseInfo(
-            string externalId,
-            string internalId,
-            string detailSchemaName,
-            string detailTableName,
-            string detailJoinColumnName,
-            string mainSchemaName,
-            string mainTableName,
-            string mainJoinColumnName)
-        {
-            ExternalId = externalId;
-            InternalId = internalId;
-            DetailSchemaName = detailSchemaName;
-            DetailTableName = detailTableName;
-            DetailJoinColumnName = detailJoinColumnName;
-            MainSchemaName = mainSchemaName;
-            MainTableName = mainTableName;
-            MainJoinColumnName = mainJoinColumnName;
-        }
-    }
 }
---
+++ src/Be.Vlaanderen.Basisregisters.Projector/Handlers/EventDatabaseInfo.cs
@@ -0,0 +1,40 @@
+namespace Be.Vlaanderen.Basisregisters.Projector.Handlers
+{
+    internal class EventDatabaseInfo
+    {
+        public string? ExternalId { get; }
+        public string? InternalId { get; }
+        public string? DetailSchemaName { get; }
+        public string? DetailTableName { get; }
+        public string? DetailJoinColumnName { get; }
+        public string? MainSchemaName { get; }
+        public string? MainTableName { get; }
+        public string? MainJoinColumnName { get; }
+        public string? Sql { get; }
+
+        public EventDatabaseInfo(
+            string externalId,
+            string internalId,
+            string detailSchemaName,
+            string detailTableName,
+            string detailJoinColumnName,
+            string mainSchemaName,
+            string mainTableName,
+            string mainJoinColumnName)
+        {
+            ExternalId = externalId;
+            InternalId = internalId;
+            DetailSchemaName = detailSchemaName;
+            DetailTableName = detailTableName;
+            DetailJoinColumnName = detailJoinColumnName;
+            MainSchemaName = mainSchemaName;
+            MainTableName = mainTableName;
+            MainJoinColumnName = mainJoinColumnName;
+        }
+
+        public EventDatabaseInfo(string sql)
+        {
+            Sql = sql;
+        }
+    }
+}
