cb5949538fd30ca307021c37450b29c800ac6ae1
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-12-31 12:25:48 +0100

    fix: re-enable Microsoft.DependencyInjection

--- src/Be.Vlaanderen.Basisregisters.Projector/Modules/ProjectorModule.cs
+++ src/Be.Vlaanderen.Basisregisters.Projector/Modules/ProjectorModule.cs
@@ -2,7 +2,6 @@ namespace Be.Vlaanderen.Basisregisters.Projector.Modules
 {
     using Autofac;
     using Autofac.Core;
-    using DependencyInjection;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Logging;
@@ -13,10 +12,10 @@ namespace Be.Vlaanderen.Basisregisters.Projector.Modules
     using IntAutofacConfiguration = Internal.Configuration;
     using IntAutofacRunners = Internal.Runners;
     using IntAutofacStreamGapStrategies = Internal.StreamGapStrategies;
-    //using IntMicrosoftCommands = InternalMicrosoft.Commands;
-    //using IntMicrosoftConfiguration = InternalMicrosoft.Configuration;
-    //using IntMicrosoftRunners = InternalMicrosoft.Runners;
-    //using IntMicrosoftStreamGapStrategies = InternalMicrosoft.StreamGapStrategies;
+    using IntMicrosoftCommands = InternalMicrosoft.Commands;
+    using IntMicrosoftConfiguration = InternalMicrosoft.Configuration;
+    using IntMicrosoftRunners = InternalMicrosoft.Runners;
+    using IntMicrosoftStreamGapStrategies = InternalMicrosoft.StreamGapStrategies;
 
     public class ProjectorModule : Module//, IServiceCollectionModule
     {
@@ -84,44 +83,44 @@ namespace Be.Vlaanderen.Basisregisters.Projector.Modules
                 (info, _) => info.ParameterType == typeof(IntAutofacStreamGapStrategies.IStreamGapStrategy),
                 (_, context) => context.Resolve<TGapStrategy>());
 
-        //public void Load(IServiceCollection services)
-        //{
-        //    services
-        //        .AddTransient<IClock>(_ => SystemClock.Instance)
-        //        .AddSingleton<IntMicrosoftCommands.IConnectedProjectionsCommandBusHandlerConfiguration, IntMicrosoftCommands.ConnectedProjectionsCommandBus>()
-        //        .AddSingleton<IntMicrosoftCommands.IConnectedProjectionsCommandBus, IntMicrosoftCommands.ConnectedProjectionsCommandBus>()
-        //        .AddSingleton<InternalMicrosoft.IMigrationHelper, InternalMicrosoft.MigrationHelper>()
-        //        .AddSingleton<InternalMicrosoft.IRegisteredProjections, InternalMicrosoft.RegisteredProjections>()
-        //        .AddTransient(typeof(InternalMicrosoft.IConnectedProjectionContext<>), typeof(InternalMicrosoft.ConnectedProjectionContext<>))
-        //        .AddSingleton<IntMicrosoftRunners.IConnectedProjectionsStreamStoreSubscription, IntMicrosoftRunners.ConnectedProjectionsStreamStoreSubscription>();
+        public void Load(IServiceCollection services)
+        {
+            services
+                .AddTransient<IClock>(_ => SystemClock.Instance)
+                .AddSingleton<IntMicrosoftCommands.IConnectedProjectionsCommandBusHandlerConfiguration, IntMicrosoftCommands.ConnectedProjectionsCommandBus>()
+                .AddSingleton<IntMicrosoftCommands.IConnectedProjectionsCommandBus, IntMicrosoftCommands.ConnectedProjectionsCommandBus>()
+                .AddSingleton<InternalMicrosoft.IMigrationHelper, InternalMicrosoft.MigrationHelper>()
+                .AddSingleton<InternalMicrosoft.IRegisteredProjections, InternalMicrosoft.RegisteredProjections>()
+                .AddTransient(typeof(InternalMicrosoft.IConnectedProjectionContext<>), typeof(InternalMicrosoft.ConnectedProjectionContext<>))
+                .AddSingleton<IntMicrosoftRunners.IConnectedProjectionsStreamStoreSubscription, IntMicrosoftRunners.ConnectedProjectionsStreamStoreSubscription>();
 
-        //    var streamGapStrategySettings = new IntMicrosoftConfiguration.StreamGapStrategyConfigurationSettings();
-        //    _configuration.Bind("streamGapStrategy", streamGapStrategySettings);
-        //    services.AddTransient<IntMicrosoftConfiguration.IStreamGapStrategyConfigurationSettings>(_ => streamGapStrategySettings);
+            var streamGapStrategySettings = new IntMicrosoftConfiguration.StreamGapStrategyConfigurationSettings();
+            _configuration.Bind("streamGapStrategy", streamGapStrategySettings);
+            services.AddTransient<IntMicrosoftConfiguration.IStreamGapStrategyConfigurationSettings>(_ => streamGapStrategySettings);
 
-        //    typeof(IntMicrosoftStreamGapStrategies.IStreamGapStrategy).Assembly
-        //        .GetTypes()
-        //        .Where(x => x.IsAssignableTo<IntMicrosoftStreamGapStrategies.IStreamGapStrategy>())
-        //        .ToList()
-        //        .ForEach(x => services.AddTransient(x));
+            typeof(IntMicrosoftStreamGapStrategies.IStreamGapStrategy).Assembly
+                .GetTypes()
+                .Where(x => x.IsAssignableTo<IntMicrosoftStreamGapStrategies.IStreamGapStrategy>())
+                .ToList()
+                .ForEach(x => services.AddTransient(x));
 
-        //    services.AddSingleton<IntMicrosoftRunners.IConnectedProjectionsSubscriptionRunner, IntMicrosoftRunners.ConnectedProjectionsSubscriptionRunner>(_ => new IntMicrosoftRunners.ConnectedProjectionsSubscriptionRunner(
-        //            _.GetRequiredService<InternalMicrosoft.IRegisteredProjections>(),
-        //            _.GetRequiredService<IntMicrosoftRunners.IConnectedProjectionsStreamStoreSubscription>(),
-        //            _.GetRequiredService<IntMicrosoftCommands.IConnectedProjectionsCommandBus>(),
-        //            _.GetRequiredService<IntMicrosoftStreamGapStrategies.DefaultSubscriptionStreamGapStrategy>(),
-        //            _.GetRequiredService<ILoggerFactory>()));
+            services.AddSingleton<IntMicrosoftRunners.IConnectedProjectionsSubscriptionRunner, IntMicrosoftRunners.ConnectedProjectionsSubscriptionRunner>(_ => new IntMicrosoftRunners.ConnectedProjectionsSubscriptionRunner(
+                    _.GetRequiredService<InternalMicrosoft.IRegisteredProjections>(),
+                    _.GetRequiredService<IntMicrosoftRunners.IConnectedProjectionsStreamStoreSubscription>(),
+                    _.GetRequiredService<IntMicrosoftCommands.IConnectedProjectionsCommandBus>(),
+                    _.GetRequiredService<IntMicrosoftStreamGapStrategies.DefaultSubscriptionStreamGapStrategy>(),
+                    _.GetRequiredService<ILoggerFactory>()));
 
-        //    services.AddSingleton<IntMicrosoftRunners.IConnectedProjectionsCatchUpRunner, IntMicrosoftRunners.ConnectedProjectionsCatchUpRunner>(_ => new IntMicrosoftRunners.ConnectedProjectionsCatchUpRunner(
-        //            _.GetRequiredService<InternalMicrosoft.IRegisteredProjections>(),
-        //            _.GetRequiredService<IReadonlyStreamStore>(),
-        //            _.GetRequiredService<IntMicrosoftCommands.IConnectedProjectionsCommandBus>(),
-        //            _.GetRequiredService<IntMicrosoftStreamGapStrategies.DefaultSubscriptionStreamGapStrategy>(),
-        //            _.GetRequiredService<ILoggerFactory>()));
+            services.AddSingleton<IntMicrosoftRunners.IConnectedProjectionsCatchUpRunner, IntMicrosoftRunners.ConnectedProjectionsCatchUpRunner>(_ => new IntMicrosoftRunners.ConnectedProjectionsCatchUpRunner(
+                    _.GetRequiredService<InternalMicrosoft.IRegisteredProjections>(),
+                    _.GetRequiredService<IReadonlyStreamStore>(),
+                    _.GetRequiredService<IntMicrosoftCommands.IConnectedProjectionsCommandBus>(),
+                    _.GetRequiredService<IntMicrosoftStreamGapStrategies.DefaultSubscriptionStreamGapStrategy>(),
+                    _.GetRequiredService<ILoggerFactory>()));
 
-        //    services.AddSingleton<IntMicrosoftCommands.IConnectedProjectionsCommandHandler, IntMicrosoftCommands.ConnectedProjectionsCommandHandler>();
+            services.AddSingleton<IntMicrosoftCommands.IConnectedProjectionsCommandHandler, IntMicrosoftCommands.ConnectedProjectionsCommandHandler>();
 
-        //    services.AddSingleton<ConnectedProjectionsMicrosoft.IConnectedProjectionsManager, InternalMicrosoft.ConnectedProjectionsManager>();
-        //}
+            services.AddSingleton<ConnectedProjectionsMicrosoft.IConnectedProjectionsManager, InternalMicrosoft.ConnectedProjectionsManager>();
+        }
     }
 }
