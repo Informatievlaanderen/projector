eba91ef5df7836cb9e767a61bab1ec742da2a22b
Arne Dumarey <dumarey.arne@gmail.com>
2023-12-14 18:44:17 +0100

    feat: bump ProjectionHandling packages

--- test/Be.Vlaanderen.Basisregisters.Projector.TestProjections/Projections/ProcessedEvent.cs
+++ test/Be.Vlaanderen.Basisregisters.Projector.TestProjections/Projections/ProcessedEvent.cs
@@ -19,8 +19,7 @@ namespace Be.Vlaanderen.Basisregisters.Projector.TestProjections.Projections
         public void Configure(EntityTypeBuilder<ProcessedEvent> b)
         {
             b.ToTable(TableName, Schemas.Projections)
-                .HasKey(x => x.Id)
-                .IsClustered(false);
+                .HasKey(x => x.Id);
 
             b.Property(x => x.Event);
             b.Property(x => x.Position);
